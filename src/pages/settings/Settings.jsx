159c6f072598d786a1576388e9fb2834c809c5c8
marizoo <marizoo@meow.woof>
2021-11-18 15:27:20 +0700

    completed settings page

---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
+import './settings.css'
+
+const Settings = () => {
+    return (
+        <div className="settings">
+            <div className="settingsWrapper">
+                <div className="settingsTitle">
+                    <span className="settingsUpdateTitle">Update Your Account</span>
+                    <span className="settingsDeleteTitle">Delete Your Account</span>
+                </div>
+
+                <form className="settingsForm">
+                    <label>Profile Picture</label>
+                    <div className="settingsPP">
+                        <img src="https://images.pexels.com/photos/1550913/pexels-photo-1550913.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+                    <label htmlFor="fileInput">
+                    <i className="settingsPPIcon far fa-user-circle"></i>
+                    </label>
+                    <input type="file" id="fileInput" style={{display: "none"}}/>
+                    </div>
+                    <label>Username</label>
+                    <input type="text" placeholder="Eva"/>
+                    <label>Email</label>
+                    <input type="email" placeholder="eva@meow.com"/>
+                    <label>Password</label>
+                    <input type="password" />
+                    <button className="settingsSubmit">Update</button>
+                </form>
+            </div>
+                <Sidebar />
+        </div>
+    )
+}
+
+export default Settings
